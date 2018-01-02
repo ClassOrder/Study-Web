acf8c7e7181ecfc71b06df79077254d318c592af
Emrys <32980429+GodfatherEmrys@users.noreply.github.com>
2018-01-02 17:58:52 +0900

    Update closerTest.js

--- JS/closerTest.js
+++ JS/closerTest.js
@@ -10,7 +10,7 @@ function toWeirdCase(s){
     return result;
 }
 
-// let을 활용했을 경우에는 클로저 부분에서 에러 발생.   
+// let을 활용했을 경우에는 내부 클로저 => 외부 함수 부분에서 에러 발생.   
 
 // 아래는 테스트로 출력해 보기 위한 코드입니다.
 console.log("결과 : " + toWeirdCase("try hello world"));
