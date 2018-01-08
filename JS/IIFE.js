a7e1c203489bef0babf44be235f40446278cb8a7
Sherlock E. Gift <32980429+GodfatherEmrys@users.noreply.github.com>
2018-01-08 09:44:31 +0000

    Update IIFE.js

--- JS/IIFE.js
+++ JS/IIFE.js
@@ -10,3 +10,16 @@
 
 // IIFE의 경우 test 변수가 전역으로 생성되지 않음을 확인해볼 수 있다.
 // 현재 스코프에 생성되지 않게 하려면 IIFE 패턴을 쓰면 된다.
+
+var File = function() {
+    var UNTITLED = ''untitled';
+    function File(name) {
+        this.name = name || UNTITLED;
+    }
+    return File;
+}();
+
+var x = 23;
+(function (twice) {
+    console.log(twice);
+}(x * 2));
