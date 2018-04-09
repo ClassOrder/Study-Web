fa33db40884b57d8eeaf16faeda0a36240fa30b0
qoov <emrys8128@gmail.com>
2018-04-09 18:11:51 +0900

    Check up Javascript

--- JS/ES6/iterator3.js
+++ JS/ES6/iterator3.js
@@ -5,3 +5,6 @@ console.log("1: ", typeof iteratorObj);
 console.log("2: ", iteratorObj.next());
 console.log("3: ", iteratorObj.next());
 console.log("4: ", iteratorObj.next());
+console.log("5: ", iteratorObj.next());
+
+/* iterator를 활용하는 것은 for문으로 반복처리하는 것과는 그 목적과 활용이 다른 것에 주의해야 합니다. */
